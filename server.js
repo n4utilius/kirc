b60752a6f4dd17b907c7d41bca0ce8c7d9fb77f8
Kevin Martinez <n4utilius@gmail.com>
2017-08-24 15:34:33 -0500

    irc inicial

--- server.js
+++ server.js
@@ -3,19 +3,24 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+var serveStatic = require('serve-static');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
+  .use(serveStatic(path.join(__dirname, 'public')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
-
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
+
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
