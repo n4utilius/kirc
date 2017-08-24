e35d0054bf7cc9445ad199e4a026b32ceac9b659
Kevin Martinez <n4utilius@gmail.com>
2017-08-24 18:57:04 -0500

    soporte multicuentas en un solo canal

--- server.js
+++ server.js
@@ -16,11 +16,35 @@ const server = express()
 const io = socketIO(server);
 io.on('connection', (socket) => {
   
+  var channels = {
+    "#n4chat": { 
+      users: ["n4"]
+    }
+  }
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on('login', (d) => {
+    if(!(d.channel in channels)){
+      io.to(socket.id).emit('login error', {msg: "This channel doesn't exist"});
+    }else{
+      if( channels[d.channel].users.indexOf(d.nick) > -1 ){
+        io.to(socket.id).emit('login error', {msg: "Other user is using this nick, try another"});
+      }else{
+        channels[d.channel].users.push(d.nick)
+        io.to(socket.id).emit('login ok', {user: { nick: d.nick, color: d.color, id: socket.id }, msg: "Log in!!!"});
+        
+        setTimeout(function(){ 
+          io.emit('chat message', { nick: "n4", msg: "Bienvenid@ " + d.nick+ "!!!", color: "#d00" });
+        }, 2500)
+      }
+    }
+  });  
+
+  //socket.emit('get channels', channels);
+  //socket.emit('get users', channels["#n4chat"] );
+
+
+
+  socket.on('chat message', (d) => {
+    io.emit('chat message', d);
   });
 });
-
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
